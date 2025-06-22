84931e2835025a2d684969912d9a190022b479f0
Lucy <lucy@absolucy.moe>
2025-06-21 21:32:26 -0400

    meow

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
 pub mod env;
 
-use auxtools::{Proc, Value, hook, raw_types::procs::ProcId};
+use auxtools::{List, Proc, Value, hook, raw_types::procs::ProcId, runtime};
 use dmasm::disassembler::DisassembleError;
+
+#[hook("/proc/all_proc_names")]
+pub fn all_proc_names() {
+	let list = List::new();
+	let mut idx = 0_u32;
+	while let Some(proc) = Proc::from_id(ProcId(idx)) {
+		idx += 1;
+		let name = Value::from_string(proc.path)?;
+		list.append(name);
+	}
+	Ok(Value::from(list))
+}
+
+#[hook("/proc/dump_proc")]
+pub fn dump_proc(name: Value) {
+	let name = name.as_string()?;
+	let proc = Proc::find(&name).ok_or_else(|| runtime!("could not find {}", name))?;
+	let mut env = env::AuxtoolsDisassembleEnv;
+	let bytecode = unsafe { proc.bytecode() }.to_vec();
+	let (nodes, _error) = dmasm::disassembler::disassemble(&bytecode, &mut env);
+	let dism = dmasm::format_disassembly(&nodes, None);
+	Value::from_string(dism)
+}
 
 #[hook("/proc/dump_disms")]
 pub fn dump_disms() {
